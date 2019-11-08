815647b0e99204d490e8d4887be8333d213c16cb
takolee <tako.lee@foxmail.com>
2019-11-08 16:27:36 +0800

    new test cases for qualify clause of teradata

---
+++ src/test/java/teradata/testQualifyClause.java
@@ -0,0 +1,23 @@
+package teradata;
+
+import gudusoft.gsqlparser.EDbVendor;
+import gudusoft.gsqlparser.ESqlStatementType;
+import gudusoft.gsqlparser.TGSqlParser;
+import gudusoft.gsqlparser.stmt.TSelectSqlStatement;
+import junit.framework.TestCase;
+
+public class testQualifyClause extends TestCase {
+
+    public void test1(){
+
+        TGSqlParser sqlparser = new TGSqlParser(EDbVendor.dbvteradata);
+        sqlparser.sqltext = "select department_id, COUNT(department_id) as department_id_number, ROW_NUMBER() OVER (ORDER BY department_id ASC), SUM(department_id_number) OVER (ORDER BY department_id ASC) sum_dept_num\n" +
+                "from foodmart.trimmed_employee GROUP by department_id qualify sum_dept_num > 0;";
+        //System.out.println(sqlparser.sqlstatements.size());
+        assertTrue(sqlparser.parse() == 0);
+        assertTrue(sqlparser.sqlstatements.get(0).sqlstatementtype == ESqlStatementType.sstselect);
+        TSelectSqlStatement select = (TSelectSqlStatement)sqlparser.sqlstatements.get(0);
+        assertTrue(select.getQualifyClause().getSearchConditoin().toString().equalsIgnoreCase("sum_dept_num > 0"));
+    }
+
+}
