7cc402956932ac5ce06a5d71e55028080797aeef
takolee <tako.lee@foxmail.com>
2020-06-08 15:27:27 +0800

    snowflake testcase

--- src/test/java/testSQLFiles/testSnowflakeFiles.java
+++ src/test/java/testSQLFiles/testSnowflakeFiles.java
@@ -3,8 +3,11 @@ package testSQLFiles;
 import gudusoft.gsqlparser.EDbVendor;
 import junit.framework.TestCase;
 
+import static testSQLFiles.parseTest.parsefiles;
+
 public class testSnowflakeFiles extends TestCase {
     public  void testSnowflake(){
         parseTest.parsefiles(EDbVendor.dbvsnowflake,test.gspCommon.BASE_SQL_DIR+"snowflake");
+        parsefiles(EDbVendor.dbvdb2,test.gspCommon.BASE_SQL_DIR+"java/snowflake/");
     }
 }
