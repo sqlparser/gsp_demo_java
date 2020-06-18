b8b1790c0ed00ab3985a66224b0730c6df8ea96a
takolee <tako.lee@foxmail.com>
2020-06-18 16:37:50 +0800

    snowflake test bug fix'

--- src/test/java/testSQLFiles/testSnowflakeFiles.java
+++ src/test/java/testSQLFiles/testSnowflakeFiles.java
@@ -8,6 +8,6 @@ import static testSQLFiles.parseTest.parsefiles;
 public class testSnowflakeFiles extends TestCase {
     public  void testSnowflake(){
         parseTest.parsefiles(EDbVendor.dbvsnowflake,test.gspCommon.BASE_SQL_DIR+"snowflake");
-        parsefiles(EDbVendor.dbvdb2,test.gspCommon.BASE_SQL_DIR+"java/snowflake/");
+        parsefiles(EDbVendor.dbvsnowflake,test.gspCommon.BASE_SQL_DIR+"java/snowflake/");
     }
 }
