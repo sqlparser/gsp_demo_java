d9af0aad9d31770557dfd0e4275efd2882cd35ab
cnfree <Actuate0>
2020-08-25 22:39:52 +0800

    支持dataflow.xml文件压缩

--- src/main/java/demos/dlineage/util/XML2Model.java
+++ src/main/java/demos/dlineage/util/XML2Model.java
@@ -1,12 +1,19 @@
 
 package demos.dlineage.util;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.StringWriter;
+import java.util.zip.Deflater;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
@@ -22,14 +29,34 @@ public class XML2Model {
 			return null;
 		}
 	}
-	
+
 	public static <T> T loadXML(Class<T> t, File file) {
 		Serializer serializer = new Persister();
+		ZipInputStream zis = null;
+		BufferedReader reader = null;
 		try {
-			return serializer.read(t, file);
+			zis = new ZipInputStream(new FileInputStream(file));
+			zis.getNextEntry();
+			reader = new BufferedReader(new InputStreamReader(zis, "UTF-8"));
+			return serializer.read(t, reader);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (zis != null) {
+				try {
+					zis.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
@@ -49,8 +76,12 @@ public class XML2Model {
 	public static <T> void saveXML(T t, File file) {
 		Serializer serializer = new Persister();
 		BufferedWriter writer = null;
+		ZipOutputStream zos = null;
 		try {
-			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF-8"));
+			zos = new ZipOutputStream(new FileOutputStream(file));
+			zos.setLevel(2);
+			zos.putNextEntry(new ZipEntry("data"));
+			writer = new BufferedWriter(new OutputStreamWriter(zos, "UTF-8"));
 			serializer.write(t, writer);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -58,6 +89,14 @@ public class XML2Model {
 			if (writer != null) {
 				try {
 					writer.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (zos != null) {
+				try {
+					zos.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
