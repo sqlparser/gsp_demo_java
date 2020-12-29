0005831a7217b4c288072948006a169c87e3fff0
shenhuan1006 <33363287+shenhuan1006@users.noreply.github.com>
2020-12-29 19:32:12 +0800

    Update testLateralView.java

--- src/test/java/common/testLateralView.java
+++ src/test/java/common/testLateralView.java
@@ -8,27 +8,26 @@ import junit.framework.TestCase;
 
 public class testLateralView extends TestCase {
 
-    public void testLateralViewClause() {
-//        TGSqlParser tgSqlParser = new TGSqlParser(EDbVendor.dbvhive);
-//
-//        tgSqlParser.sqltext = "SELECT pageid, adid FROM pageAds LATERAL VIEW explode(adid_list) adTable AS adid";
-//
-//        assertTrue(tgSqlParser.parse() == 0);
-//        TSelectSqlStatement sqlstatements = (TSelectSqlStatement) tgSqlParser.getSqlstatements().get(0);
-//        TTableList tables = sqlstatements.getTables();
-//        TPTNodeList<THiveLateralView> lateralViewList = tables.getTable(0).getLateralViewList();
-//        assertTrue(lateralViewList.toString().equalsIgnoreCase("LATERAL VIEW explode(adid_list) adTable AS adid"));
-//        assertTrue(lateralViewList.getElement(0).toString().equalsIgnoreCase("LATERAL VIEW explode(adid_list) adTable AS adid"));
-//
-//        TFunctionCall tFunctionCall = lateralViewList.getElement(0).getUdtf();
-//        assertTrue(tFunctionCall.toString().equalsIgnoreCase("explode(adid_list)"));
-//
-//        TAliasClause tableAlias = lateralViewList.getElement(0).getTableAlias();
-//        assertTrue(tableAlias.toString().equalsIgnoreCase("adTable"));
-//
-//        TObjectNameList columnAliasList = lateralViewList.getElement(0).getColumnAliasList();
-//        TObjectName element = (TObjectName) columnAliasList.getElement(0);
-//        assertTrue(element.toString().equalsIgnoreCase("adid"));
+   public void testLateralViewClause() {
+        TGSqlParser tgSqlParser = new TGSqlParser(EDbVendor.dbvhive);
+
+        tgSqlParser.sqltext = "SELECT pageid, adid FROM pageAds LATERAL VIEW explode(adid_list) adTable AS adid";
+
+        assertTrue(tgSqlParser.parse() == 0);
+        TSelectSqlStatement sqlstatements = (TSelectSqlStatement) tgSqlParser.getSqlstatements().get(0);
+        TTableList tables = sqlstatements.getTables();
+        ArrayList<THiveLateralView> lateralViewList = tables.getTable(0).getLateralViewList();
+        assertTrue(lateralViewList.get(0).toString().equalsIgnoreCase("LATERAL VIEW explode(adid_list) adTable AS adid"));
+
+        TFunctionCall tFunctionCall = lateralViewList.get(0).getUdtf();
+        assertTrue(tFunctionCall.toString().equalsIgnoreCase("explode(adid_list)"));
+
+        TAliasClause tableAlias = lateralViewList.get(0).getTableAlias();
+        assertTrue(tableAlias.toString().equalsIgnoreCase("adTable"));
+
+        TObjectNameList columnAliasList = lateralViewList.get(0).getColumnAliasList();
+        TObjectName element = (TObjectName) columnAliasList.getElement(0);
+        assertTrue(element.toString().equalsIgnoreCase("adid"));
     }
 
 }
