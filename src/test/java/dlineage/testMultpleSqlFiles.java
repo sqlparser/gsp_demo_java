231312b7759890b8a66878564500fed1d10721ea
cnfree <Actuate0>
2021-02-22 21:10:58 +0800

    Large DB2 DDLs takes several hours before completing #555

---
+++ src/test/java/dlineage/testMultpleSqlFiles.java
@@ -0,0 +1,62 @@
+package dlineage;
+
+import java.io.File;
+import java.util.List;
+
+import gudusoft.gsqlparser.EDbVendor;
+import gudusoft.gsqlparser.dlineage.DataFlowAnalyzer;
+import gudusoft.gsqlparser.dlineage.dataflow.model.DbObjectPosition;
+import gudusoft.gsqlparser.dlineage.dataflow.model.json.Coordinate;
+import gudusoft.gsqlparser.dlineage.dataflow.model.xml.dataflow;
+import gudusoft.gsqlparser.dlineage.dataflow.model.xml.procedure;
+import gudusoft.gsqlparser.dlineage.dataflow.model.xml.table;
+import gudusoft.gsqlparser.dlineage.util.Pair;
+import gudusoft.gsqlparser.dlineage.util.SqlInfoHelper;
+import junit.framework.TestCase;
+
+public class testMultpleSqlFiles extends TestCase {
+
+	public void test1() {
+		//File sqlfiles = new File("D:/develop/git/gsp_sqlfiles/TestCases/dlineage/mysql/547");
+		File sqlfiles = new File("c:/prg/gsqlparser/Test/TestCases/dlineage/mysql/547");
+		DataFlowAnalyzer analyzer = new DataFlowAnalyzer(sqlfiles, EDbVendor.dbvmysql, false);
+		analyzer.generateDataFlow();
+		dataflow dataflow = analyzer.getDataFlow();
+		String sqlInfoJson = SqlInfoHelper.getSqlInfoJson(analyzer);
+		SqlInfoHelper helper = new SqlInfoHelper(sqlInfoJson);
+
+		table basic_vw = dataflow.getViews().get(0);
+		Coordinate[][] viewCoordinates = SqlInfoHelper.parseCoordinateString(basic_vw.getCoordinate());
+
+		DbObjectPosition viewPosition = helper.getSelectedDbObjectInfo(viewCoordinates[0][0], viewCoordinates[0][1]);
+		String fileName = viewPosition.getFile();
+		List<Pair<Integer, Integer>> positions = viewPosition.getPositions();
+		assertEquals(fileName, "1.sql");
+		assertEquals(positions.get(0), new Pair<Integer, Integer>(1, 13));
+		assertEquals(positions.get(1), new Pair<Integer, Integer>(1, 23));
+
+		procedure Basic_Sal = dataflow.getProcedures().get(0);
+		Coordinate[][] procedureCoordinates = SqlInfoHelper.parseCoordinateString(Basic_Sal.getCoordinate());
+
+		DbObjectPosition procedurePosition = helper.getSelectedDbObjectInfo(procedureCoordinates[0][0],
+				procedureCoordinates[0][1]);
+		fileName = procedurePosition.getFile();
+		positions = procedurePosition.getPositions();
+		assertEquals(fileName, "2.sql");
+		assertEquals(positions.get(0), new Pair<Integer, Integer>(3, 18));
+		assertEquals(positions.get(1), new Pair<Integer, Integer>(3, 29));
+
+		DbObjectPosition procedureStatementPosition = helper.getSelectedDbObjectStatementInfo(EDbVendor.dbvmysql,
+				procedureCoordinates[0][0], procedureCoordinates[0][1]);
+		String sql = procedureStatementPosition.getSql();
+		String stmt = "CREATE PROCEDURE `Basic_Sal`()BEGIN\r\n" +
+				"        SELECT * FROM basic;\r\n" +
+				"END;";
+		int index = procedureStatementPosition.getIndex();
+		positions = procedureStatementPosition.getPositions();
+		assertEquals(index, 1);
+		assertEquals(sql, stmt);
+		assertEquals(positions.get(0), new Pair<Integer, Integer>(1, 18));
+		assertEquals(positions.get(1), new Pair<Integer, Integer>(1, 29));
+	}
+}
