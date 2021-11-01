b20e27b919a421111e4f3da746159ec526a3f9c8
chenchao <chao.chen@pscsoft.com.cn>
2021-11-01 10:34:55 +0800

    Update testcases.

--- src/test/java/demos/analyzespTest.java
+++ src/test/java/demos/analyzespTest.java
@@ -15,7 +15,7 @@ public class analyzespTest extends TestCase
 
 	protected void setUp( )
 	{
-		basedir = common.gspCommon.BASE_SQL_DIR+"sqlscripts/analyze_sp";
+		basedir = common.gspCommon.BASE_SQL_DIR+"private/sqlscripts/analyze_sp";
 	}
 
 	public void testSample1( )
