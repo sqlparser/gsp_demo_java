ed63b1c17b920bac0dfa4310ee0b8327c3c5cd37
shenhuan2021 <33363287+shenhuan2021@users.noreply.github.com>
2022-02-16 20:42:27 +0800

    Update testColumnLevelLineageFile.java

--- src/test/java/dlineage/testColumnLevelLineageFile.java
+++ src/test/java/dlineage/testColumnLevelLineageFile.java
@@ -88,7 +88,7 @@ public class testColumnLevelLineageFile extends TestCase {
         assertTrue(compare(common.gspCommon.BASE_SQL_DIR_PUBLIC + "lineage/insert-create.sql", common.gspCommon.BASE_SQL_DIR_PUBLIC + "lineage/insert-create.csv", EDbVendor.dbvoracle));
     }
 
-public void test20() {
+    public void test20() {
         assertTrue(compare(common.gspCommon.BASE_SQL_DIR_PUBLIC + "lineage/insert-select.sql", common.gspCommon.BASE_SQL_DIR_PUBLIC + "lineage/insert-select.csv", EDbVendor.dbvoracle));
     }
     
