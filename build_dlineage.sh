d4fcf8489d385058d6b09cf5e3e7a57c5ca12465
shenhuan2021 <33363287+shenhuan2021@users.noreply.github.com>
2022-11-02 22:46:34 +0800

    Update build_dlineage.sh

--- build_dlineage.sh
+++ build_dlineage.sh
@@ -13,39 +13,39 @@ version=$ps2
 
 echo 'start build'
 cd $p1
-git clone https://shenhuan2021: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@github.com/sqlparser/dlineage.git
-sudo mkdir -p $p1/dlineage/lib/
-sudo mkdir -p $p1/dlineage/src/main/java/
+git clone git@github.com:shenhuan2021/dlineage_base_base.git
+sudo mkdir -p $p1/dlineage_base/lib/
+sudo mkdir -p $p1/dlineage_base/src/main/java/demos/lineage
 
-sudo cp $p1/lib/*.jar $p1/dlineage/lib/
-sudo cp $p1/external_lib/sqlflow-exporter.jar $p1/dlineage/lib/
-sudo cp $p1/src/main/java/demos/dlineage/DataFlowAnalyzer.java $p1/dlineage/src/main/java/
-sudo cp $p1/src/main/java/demos/dlineage/SqlflowIngester.java $p1/dlineage/src/main/java/
-cd dlineage
-chmod 777 maven.sh
+sudo cp $p1/lib/*.jar $p1/dlineage_base/lib/
+sudo cp $p1/external_lib/sqlflow-exporter.jar $p1/dlineage_base/lib/
+sudo cp $p1/src/main/java/demos/dlineage_base/DataFlowAnalyzer.java $p1/dlineage_base/src/main/java/demos/lineage/
+sudo cp $p1/src/main/java/demos/dlineage_base/SqlflowIngester.java $p1/dlineage_base/src/main/java/demos/lineage/
+cd dlineage_base
+sudo chmod 777 maven.sh
 sh maven.sh
-echo 'maven dlineage jar success.'
+echo 'maven dlineage_base jar success.'
 
 cd $p1
-sudo mkdir -p $p1/Dlineage/lib
-sudo mkdir -p $p1/Dlineage/src
-sudo mkdir -p $p1/Dlineage/bin
-sudo mkdir -p $p1/Dlineage/doc
-sudo mkdir -p $p1/Dlineage/simple
-sudo mkdir -p $p1/Dlineage/script
+sudo mkdir -p $p1/dlineage_base/lib
+sudo mkdir -p $p1/dlineage_base/src
+sudo mkdir -p $p1/dlineage_base/bin
+sudo mkdir -p $p1/dlineage_base/doc
+sudo mkdir -p $p1/dlineage_base/simple
+sudo mkdir -p $p1/dlineage_base/script
 echo 'mkdir success.'
 
-sudo cp $p1/dlineage/target/*.jar $p1/Dlineage/bin/
-sudo cp $p1/dlineage/script/start.sh $p1/Dlineage/script/
-sudo cp $p1/dlineage/script/start.bat $p1/Dlineage/script/
-sudo cp $p1/lib/*.jar $p1/Dlineage/lib/
-sudo cp $p1/src/main/java/demos/dlineage/DataFlowAnalyzer.java $p1/Dlineage/src/
-sudo cp $p1/src/main/java/demos/dlineage/SqlflowIngester.java $p1/Dlineage/src/
-sudo cp $p1/src/main/java/demos/dlineage/sample/*.sql $p1/Dlineage/sample/
-sudo cp $p1/dlineage/sqlflow-data-lineage-model-reference.pdf $p1/Dlineage/doc/
-sudo cp $p1/src/main/java/demos/dlineage/readme.md $p1/Dlineage/
+sudo cp $p1/dlineage_base/target/*.jar $p1/dlineage_base/bin/
+sudo cp $p1/dlineage_base/script/start.sh $p1/dlineage_base/script/
+sudo cp $p1/dlineage_base/script/start.bat $p1/dlineage_base/script/
+sudo cp $p1/lib/*.jar $p1/dlineage_base/lib/
+sudo cp $p1/src/main/java/demos/dlineage_base/DataFlowAnalyzer.java $p1/dlineage_base/src/
+sudo cp $p1/src/main/java/demos/dlineage_base/SqlflowIngester.java $p1/dlineage_base/src/
+sudo cp $p1/src/main/java/demos/dlineage_base/sample/*.sql $p1/dlineage_base/sample/
+sudo cp $p1/dlineage_base/sqlflow-data-lineage-model-reference.pdf $p1/dlineage_base/doc/
+sudo cp $p1/src/main/java/demos/dlineage_base/readme.md $p1/dlineage_base/
 echo 'cp gsp jar success.'
 
-sudo zip -r gudusoft.dlineage-$version.zip Dlineage/
+sudo zip -r gudusoft.dlineage_base-$version.zip dlineage_base/
 mkdir dlienagezip
-cp gudusoft.dlineage-$version.zip dlienagezip/
+cp gudusoft.dlineage_base-$version.zip dlienagezip/
