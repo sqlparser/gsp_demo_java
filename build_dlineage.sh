659c0b5e26c19283a0590df0078a8defca5d15d3
shenhuan2021 <33363287+shenhuan2021@users.noreply.github.com>
2022-11-02 22:06:16 +0800

    Create build_dlineage.sh

---
+++ build_dlineage.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#source dir
+p1=$1
+
+cd $p1/gsp_demo_java/lib
+jarpath=$(ls)
+echo 'zippath is '
+ps=(${jarpath//.jar/ })
+dirpath=${ps[0]}
+ps2=(${dirpath//gudusoft\.gsqlparser\./ })
+version=$ps2
+
+echo 'start build'
+cd $p1
+git clone https://shenhuan2021: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@github.com/sqlparser/dlineage.git
+sudo cp $p1/gsp_demo_java/lib/*.jar $p1/dlineage/lib/gudusoft.gsqlparser.jar
+sudo cp $p1/gsp_demo_java/external_lib/sqlflow-exporter.jar $p1/dlineage/lib/sqlflow-exporter.jar
+sudo cp $p1/gsp_demo_java/lib/src/main/java/demos/dlineage/DataFlowAnalyzer.java $p1/dlineage/src/main/java/DataFlowAnalyzer.java
+sudo cp $p1/gsp_demo_java/lib/src/main/java/demos/dlineage/SqlflowIngester.java $p1/dlineage/src/main/java/SqlflowIngester.java
+cd dlineage
+chmod 777 maven.sh
+sh maven.sh
+echo 'maven dlineage jar success.'
+
+cd $p1
+sudo mkdir -p $p1/Dlineage/lib
+sudo mkdir -p $p1/Dlineage/src
+sudo mkdir -p $p1/Dlineage/bin
+sudo mkdir -p $p1/Dlineage/doc
+sudo mkdir -p $p1/Dlineage/simple
+sudo mkdir -p $p1/Dlineage/script
+echo 'mkdir success.'
+
+sudo cp $p1/dlineage/target/*.jar $p1/Dlineage/bin/
+sudo cp $p1/dlineage/script/start.sh $p1/Dlineage/script/
+sudo cp $p1/dlineage/script/start.bat $p1/Dlineage/script/
+sudo cp $p1/gsp_demo_java/lib/*.jar $p1/Dlineage/lib/
+sudo cp $p1/gsp_demo_java/lib/src/main/java/demos/dlineage/DataFlowAnalyzer.java $p1/Dlineage/src/DataFlowAnalyzer.java
+sudo cp $p1/gsp_demo_java/lib/src/main/java/demos/dlineage/SqlflowIngester.java $p1/Dlineage/src/SqlflowIngester.java
+sudo cp $p1/gsp_demo_java/lib/src/main/java/demos/dlineage/sample/*.sql $p1/Dlineage/sample/
+sudo cp $p1/dlineage/sqlflow-data-lineage-model-reference.pdf $p1/Dlineage/doc/
+sudo cp $p1/gsp_demo_java/lib/src/main/java/demos/dlineage/readme.md $p1/Dlineage/
+echo 'cp gsp jar success.'
+
+sudo zip -r gudusoft.dlineage-$version.zip Dlineage/
+mkdir dlienagezip
+cp gudusoft.dlineage-$version.zip dlienagezip/
