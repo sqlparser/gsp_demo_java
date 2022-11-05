f1435cacad21e8ea31857b9fe34fdedb43f879bf
sqlparser <james@gudusoft.com>
2022-11-05 23:30:43 +0800

    2.6.1.4

--- src/test/java/databricks/testDatatypeCast.java
+++ src/test/java/databricks/testDatatypeCast.java
@@ -14,7 +14,7 @@ public class testDatatypeCast extends TestCase {
         assertTrue(sqlparser.parse() == 0);
         TSelectSqlStatement selectSqlStatement = (TSelectSqlStatement) sqlparser.sqlstatements.get (0);
         TExpression expression = selectSqlStatement.getResultColumnList().getResultColumn(0).getExpr();
-        assertTrue(expression.getExpressionType() == EExpressionType.typecast_t);
+        assertTrue(expression.getExpressionType() == EExpressionType.typecast_datatype_t);
         assertTrue(expression.getCastDatatype() == EDataType.bigint_t);
         assertTrue(expression.getLeftOperand().toString().equalsIgnoreCase("current_timestamp"));
         //System.out.println(expression.getLeftOperand().getExpressionType());
