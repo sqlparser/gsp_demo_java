59fedee3b85411f13e318b8d37600d26d0fee4a4
sqlparser <james@gudusoft.com>
2022-11-24 09:31:56 +0800

    modify of test large union

--- src/test/java/common/testLargeUnion.java
+++ src/test/java/common/testLargeUnion.java
@@ -11,6 +11,10 @@ import java.util.ArrayList;
 public class testLargeUnion extends TestCase {
 
     public static void test1(){
+        //  递归使用的是栈里面的内存，JVM默认一个线程的栈内存是1MB，单独跑这个case，
+        //  线程栈里只有这个case的内容，一起跑，栈里可能还有其它的内容，挤压了这个case可用栈内存的空间。
+        //
+        //增加JVM参数 -Xss2M 就可以解决这个问题
         TGSqlParser sqlParser = new TGSqlParser(EDbVendor.dbvsnowflake);
         sqlParser.sqlfilename = gspCommon.BASE_SQL_DIR_PRIVATE_JAVA+"snowflake/atlan/large_union.sql";
         sqlParser.parse();
