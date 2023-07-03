3341ac0768dc625a2461187062319e4bb98109de
Tako Lee <tako.lee@foxmail.com>
2023-07-03 14:25:53 +0800

    2.7.2.5

--- src/test/java/scriptWriter/testDatabricks.java
+++ src/test/java/scriptWriter/testDatabricks.java
@@ -155,4 +155,53 @@ public class testDatabricks extends TestCase
         sqlparser.parse( );
         assertTrue(testScriptGenerator.verifyScript(EDbVendor.dbvdatabricks, sqlparser.sqlstatements.get(0).toString(), sqlparser.sqlstatements.get(0).toScript()));
 	}
+
+    public void test10( )
+    {
+        TGSqlParser sqlparser = new TGSqlParser( EDbVendor.dbvdatabricks);
+        sqlparser.sqltext = "WITH base AS\n" +
+                "(\n" +
+                "  SELECT cost_category_level1,\n" +
+                "         SUM(CASE WHEN (to_date (CONCAT (hb.period_year,'-',hb.period_num,'-01'),'yyyy-MM-dd') BETWEEN to_date ('2023-04-01','yyyy-MM-dd') -INTERVAL 1 YEAR AND to_date ('2023-06-30','yyyy-MM-dd') -INTERVAL 1 YEAR) THEN hb.period_balance_amount ELSE 0 END) AS prev_year_value,\n" +
+                "         SUM(CASE WHEN (hb.period_year = YEAR(to_date ('2023-06-30','yyyy-MM-dd')) -(FLOOR((12 -MONTH(to_date ('2023-06-30','yyyy-MM-dd'))) / 9)) AND hb.period_num = (MONTH(to_date ('2023-06-30','yyyy-MM-dd')) + 8) % 12 + 1) THEN hb.qtd_balance_amount ELSE 0 END) AS prev_qtr_value,\n" +
+                "         CASE\n" +
+                "           WHEN DAY(to_date ('2023-04-01','yyyy-MM-dd')) IN ('01','1') AND MONTH(to_date ('2023-04-01','yyyy-MM-dd')) IN ('01','1') AND DAY(to_date ('2023-06-30','yyyy-MM-dd')) IN ('31') AND MONTH(to_date ('2023-06-30','yyyy-MM-dd')) IN ('03','3') AND YEAR(to_date ('2023-04-01','yyyy-MM-dd')) = YEAR(to_date ('2023-06-30','yyyy-MM-dd')) OR DAY(to_date ('2023-04-01','yyyy-MM-dd')) IN ('01','1') AND MONTH(to_date ('2023-04-01','yyyy-MM-dd')) IN ('04','4') AND DAY(to_date ('2023-06-30','yyyy-MM-dd')) IN ('30') AND MONTH(to_date ('2023-06-30','yyyy-MM-dd')) IN ('06','6') AND YEAR(to_date ('2023-04-01','yyyy-MM-dd')) = YEAR(to_date ('2023-06-30','yyyy-MM-dd')) OR DAY(to_date ('2023-04-01','yyyy-MM-dd')) IN ('01','1') AND MONTH(to_date ('2023-04-01','yyyy-MM-dd')) IN ('07','7') AND DAY(to_date ('2023-06-30','yyyy-MM-dd')) IN ('30') AND MONTH(to_date ('2023-06-30','yyyy-MM-dd')) IN ('09','9') AND YEAR(to_date ('2023-04-01','yyyy-MM-dd')) = YEAR(to_date ('2023-06-30','yyyy-MM-dd')) OR DAY(to_date ('2023-04-01','yyyy-MM-dd')) IN ('01','1') AND MONTH(to_date ('2023-04-01','yyyy-MM-dd')) IN ('10') AND DAY(to_date ('2023-06-30','yyyy-MM-dd')) IN ('31') AND MONTH(to_date ('2023-06-30','yyyy-MM-dd')) IN ('12') AND YEAR(to_date ('2023-04-01','yyyy-MM-dd')) = YEAR(to_date ('2023-06-30','yyyy-MM-dd')) THEN 'Quarterly'\n" +
+                "           WHEN DAY(to_date ('2023-04-01','yyyy-MM-dd')) IN ('01','1') AND MONTH(to_date ('2023-04-01','yyyy-MM-dd')) IN ('01','1') AND to_date ('2023-06-30','yyyy-MM-dd') = CURRENT_DATE() THEN 'YTD'\n" +
+                "           ELSE 'Custom'\n" +
+                "         END AS period,\n" +
+                "         SUM(CASE WHEN (to_date (CONCAT (hb.period_year,'-',hb.period_num,'-01'),'yyyy-MM-dd') BETWEEN to_date ('2023-04-01','yyyy-MM-dd') AND to_date ('2023-06-30','yyyy-MM-dd')) THEN hb.period_balance_amount ELSE 0 END) AS curr_value\n" +
+                "  FROM fdl_db_qa_dbr_consumption_inc.xx_hfm_balances hb\n" +
+                "    JOIN fdl_db_qa_dbr_consumption_inc.xx_drm_hier_account dha\n" +
+                "      ON hb.account = dha.name\n" +
+                "     AND dha.drm_type = 'ABC_D'\n" +
+                "     AND dha.drm_org = 'AB'\n" +
+                "    JOIN fdl_db_qa_dbr_consumption_inc.xx_drm_hier_supplementaldata dhs\n" +
+                "      ON dhs.name = hb.supplemental_dim\n" +
+                "     AND dhs.drm_type = 'ABCD'\n" +
+                "     AND dhs.drm_org = 'AB'\n" +
+                "    JOIN fdl_db_qa_dbr_consumption_inc.xx_drm_hier_reporting dhr\n" +
+                "      ON dhr.name = hb.reporting_dim\n" +
+                "     AND dhr.drm_type = 'ABCD'\n" +
+                "     AND dhr.drm_org = 'AB'\n" +
+                "    JOIN fdl_db_qa_dbr_consumption_inc.xx_gl_code_combinations xgcc\n" +
+                "      ON hb.source_system_name = xgcc.source_system_name\n" +
+                "     AND hb.code_combination_id = xgcc.CODE_COMBINATION_ID\n" +
+                "  WHERE hb.currency = 'USD'\n" +
+                "  AND   hb.scenario = 'Actual'\n" +
+                "  AND   hb.data_view = 'YTD'\n" +
+                "  AND   dhr.level03_parent_node = 'QQ_TorCrAsRettttpo'\n" +
+                "  AND   dhs.level03_parent_node = 'AB_RPEE'\n" +
+                "  AND   (to_date(CONCAT(hb.period_year,'-',hb.period_num,'-01'),'yyyy-MM-dd') BETWEEN to_date('2023-04-01','yyyy-MM-dd') -INTERVAL 1 YEAR AND to_date('2023-06-30','yyyy-MM-dd')) AND dha.level05_parent_node = 'AC_F512000000'\n" +
+                "  GROUP BY cost_category_level1\n" +
+                ")\n" +
+                "SELECT cost_category_level1,\n" +
+                "       base.curr_value AS curr_value,\n" +
+                "       IF (period LIKE 'Quarterly',COALESCE((base.curr_value - base.prev_qtr_value),0) /COALESCE(ABS(base.prev_qtr_value),1)*100,'-') AS VPQ,\n" +
+                "       IF (period IN ('Quarterly','YTD'),COALESCE((base.curr_value - base.prev_year_value),0) /COALESCE(ABS(base.prev_year_value),1)*100,'-') AS VPY\n" +
+                "FROM base\n" +
+                "WHERE (FLOOR(ABS(base.curr_value)) != 0)\n" +
+                "ORDER BY cost_category_level1";
+        sqlparser.parse( );
+        assertTrue(testScriptGenerator.verifyScript(EDbVendor.dbvdatabricks, sqlparser.sqlstatements.get(0).toString(), sqlparser.sqlstatements.get(0).toScript()));
+    }
 }
