37d92b36e89f9e5d38af185238ed93ebdeb7268d
cnfree <cnfree2000@hotmail.com>
2024-01-19 01:07:23 +0800

    update test cases

--- src/test/java/formatsql/testFunctionCall.java
+++ src/test/java/formatsql/testFunctionCall.java
@@ -8,11 +8,70 @@ import gudusoft.gsqlparser.TGSqlParser;
 import gudusoft.gsqlparser.pp.para.GFmtOpt;
 import gudusoft.gsqlparser.pp.para.GFmtOptFactory;
 import gudusoft.gsqlparser.pp.para.styleenums.TAlignStyle;
+import gudusoft.gsqlparser.pp.para.styleenums.TCaseOption;
 import gudusoft.gsqlparser.pp.para.styleenums.TLinefeedsCommaOption;
 import gudusoft.gsqlparser.pp.stmtformatter.FormatterFactory;
 import junit.framework.TestCase;
 
 public class testFunctionCall extends TestCase {
+	
+	public static void testCurrentDate1() {
+		GFmtOpt option = GFmtOptFactory.newInstance(new Exception().getStackTrace()[0].getClassName() + "."
+				+ new Exception().getStackTrace()[0].getMethodName());
+		option.alignAliasInSelectList = false;
+		option.caseKeywords = TCaseOption.CoUppercase;
+		option.caseIdentifier = TCaseOption.CoNoChange;
+		option.caseFuncname = TCaseOption.CoUppercase;
+		option.caseDatatype = TCaseOption.CoUppercase;
+		option.caseWhenThenInSameLine = true;
+		// http://www.dpriver.com/ppv3/whitespace_padding.php
+		option.wsPaddingOperatorArithmetic = true;
+		option.wsPaddingParenthesesInFunction = false;
+		option.wsPaddingParenthesesInFunctionCall = false;
+		option.wsPaddingParenthesesOfSubQuery = false;
+		option.beStyleCreatetableLeftBEOnNewline = true;
+		option.beStyleBlockIndentSize = 0;
+
+		TGSqlParser sqlparser = new TGSqlParser(EDbVendor.dbvsnowflake);
+		sqlparser.sqltext = "SELECT case when CURRENT_DATE()> current_date then TRUE else FALSE end as status";
+
+		sqlparser.parse();
+		String result = FormatterFactory.pp(sqlparser, option);
+		assertTrue(result.trim().equalsIgnoreCase("SELECT CASE\n"
+				+ "         WHEN CURRENT_DATE() > CURRENT_DATE THEN TRUE\n"
+				+ "         ELSE FALSE\n"
+				+ "       END AS status"));
+		// System.out.println(result);
+	}
+	
+	public static void testCurrentDate2() {
+		GFmtOpt option = GFmtOptFactory.newInstance(new Exception().getStackTrace()[0].getClassName() + "."
+				+ new Exception().getStackTrace()[0].getMethodName());
+		option.alignAliasInSelectList = false;
+		option.caseKeywords = TCaseOption.CoUppercase;
+		option.caseIdentifier = TCaseOption.CoNoChange;
+		option.caseFuncname = TCaseOption.CoUppercase;
+		option.caseDatatype = TCaseOption.CoUppercase;
+		option.caseWhenThenInSameLine = true;
+		// http://www.dpriver.com/ppv3/whitespace_padding.php
+		option.wsPaddingOperatorArithmetic = true;
+		option.wsPaddingParenthesesInFunction = false;
+		option.wsPaddingParenthesesInFunctionCall = false;
+		option.wsPaddingParenthesesOfSubQuery = false;
+		option.beStyleCreatetableLeftBEOnNewline = true;
+		option.beStyleBlockIndentSize = 0;
+
+		TGSqlParser sqlparser = new TGSqlParser(EDbVendor.dbvsnowflake);
+		sqlparser.sqltext = "SELECT case when current_date> CURRENT_DATE() then TRUE else FALSE end as status";
+
+		sqlparser.parse();
+		String result = FormatterFactory.pp(sqlparser, option);
+		assertTrue(result.trim().equalsIgnoreCase("SELECT CASE\n"
+				+ "         WHEN CURRENT_DATE > CURRENT_DATE() THEN TRUE\n"
+				+ "         ELSE FALSE\n"
+				+ "       END AS status"));
+		// System.out.println(result);
+	}
 
    public static void testParameters(){
          GFmtOpt option = GFmtOptFactory.newInstance(new Exception().getStackTrace()[0].getClassName() + "." + new Exception().getStackTrace()[0].getMethodName());
